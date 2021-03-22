ab85d9346925db5e9b662bab2827b0388c08f663
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-22 15:35:07 +0100

    Add components, add pages, add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+//Import Components
+import HomePage from "./pages/HomePage";
+import AddPage from "./pages/AddPage";
+import EditPage from "./pages/EditPage";
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/cards" />
+        </Route>
+        <Route path="/cards" exact>
+          <HomePage />
+        </Route>
+        <Route path="/cards/add">
+          <AddPage />
+        </Route>
+        <Route path="/cards/:id/edit">
+          <EditPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/AddPage.js
@@ -0,0 +1,11 @@
+// Add components
+
+const AddPage = () => {
+  return (
+    <div>
+      <h1>Add Page</h1>
+    </div>
+  );
+};
+
+export default AddPage;
